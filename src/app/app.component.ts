44814d0a5a32ee4919bccc0033a9f3c655b8ee85
Bilberry04 <kacper.borowiec04@gmail.com>
2025-03-25 09:25:50 +0100

    wydane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WydaneComponent } from "./wydane/wydane.component";
+import { AppNumberExampleComponent } from "./wydane/app-number-example/app-number-example.component";
+
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root', 
+  imports: [WydaneComponent, AppNumberExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'projekcik';
 }
+ 
